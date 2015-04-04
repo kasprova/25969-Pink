daad823b49c8efc654f61bcb9f66b8c4e251d567
An__Ka <kasprova_a@yahoo.com>
2015-04-04 20:47:10 +0300

    Added adaptive graphics, svg, retina, slider for review.

--- js/script.js
+++ js/script.js
@@ -7,12 +7,10 @@ function initialize() {
   }
   var map = new google.maps.Map(document.getElementById("map-canvas"),
                                 mapOptions);
-  /*var image = "..img/icon-pin.png";*/
   var myLatLng = new google.maps.LatLng(59.9360427,30.322952);
   var beachMarker = new google.maps.Marker({
     position: myLatLng,
-    map: map/*,
-    icon: image*/
+    map: map
   });
 }
 google.maps.event.addDomListener(window, 'load', initialize);
